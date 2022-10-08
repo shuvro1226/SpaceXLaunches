43a0a2b1caf1a132dfb0ed0a449b0438e1490d9b
Mainul Hasan Shuvro <mainul.shuvro@contentpepper.de>
2022-10-08 15:03:16 +0200

    Handled login timeout not being cleared properly

--- src/components/LaunchesByStatus/LaunchesByStatus.tsx
+++ src/components/LaunchesByStatus/LaunchesByStatus.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import Spinner from "react-bootstrap/Spinner";
@@ -6,12 +6,13 @@ import { STATUS_CONFIG } from "../../lib/consts";
 import Launches from "../Launches/Launches";
 import { Status } from "../../types";
 import ToastComponent from "../ToastComponent/ToastComponent";
-import styles from './LaunchByStatus.module.css';
+import styles from "./LaunchByStatus.module.css";
 
 const LaunchesByStatus = (): JSX.Element => {
   const [key, setKey] = useState<string>("success");
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [show, setShow] = useState<boolean>(false);
+  const timeoutID: { current: NodeJS.Timeout | null } = useRef(null);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -38,18 +39,19 @@ const LaunchesByStatus = (): JSX.Element => {
         setIsAuthenticated(true);
         setShow(true);
       };
-
+      
       // Adding 3 seconds delay for loggin in to show the authenticating message
-      const loginInterval = setInterval(() => login(), 5000);
-
-      return () => clearInterval(loginInterval);
+      timeoutID.current = setTimeout(() => login(), 3000);
+      return () => clearTimeout(timeoutID.current!);
     }
   }, []);
 
   if (!isAuthenticated) {
     return (
       <>
-        <h3 className="mt-5 mb-2"><Spinner animation="border" /></h3>
+        <h3 className="mt-5 mb-2">
+          <Spinner animation="border" />
+        </h3>
         <h4>Authenticating...</h4>
       </>
     );
@@ -64,13 +66,15 @@ const LaunchesByStatus = (): JSX.Element => {
         show={show}
         setShow={setShow}
       />
-      <Tabs
-        activeKey={key}
-        onSelect={(k: any): void => setKey(k)}
-      >
+      <Tabs activeKey={key} onSelect={(k: any): void => setKey(k)}>
         {STATUS_CONFIG.map((status: Status, i: number) => {
           return (
-            <Tab key={i} eventKey={status.identifier} title={status.name} className={`${styles.tabWrapper} pt-3`}>
+            <Tab
+              key={i}
+              eventKey={status.identifier}
+              title={status.name}
+              className={`${styles.tabWrapper} pt-3`}
+            >
               <Launches status={status.identifier} />
             </Tab>
           );
